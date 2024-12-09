ddfbf67ad5faef4f78f2b191fe5f1b79bd89896b
Euge <eugeniatamborelli@gmail.com>
2024-12-08 21:07:19 -0300

    Primera version de la pagina web

---
+++ utilities.js
@@ -0,0 +1,7 @@
+function toggleSection(sectionId) {
+    // Obtiene el elemento de la sección
+    var section = document.getElementById(sectionId);
+
+    // Cambia su visibilidad
+    section.classList.toggle('open');
+}
